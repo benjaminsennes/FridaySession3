dddab8fa1994b6fc2e44e9ac28e65cb236bf06f5
benjaminsennes <bsennes.ieu2023@student.ie.edu>
2025-01-17 13:21:07 +0100

    added 2 more files

---
+++ arithmetic_op.py
@@ -0,0 +1,15 @@
+print(7+11)
+print(7-11)
+print(7*11)
+print(9/3)
+print(type(9/3), 9/3)
+print(None)
+print(2**10)
+print(8//2)
+print(8%9)
+print(20%8)
+
+name = "benjamin sennes"
+print("je suis " + name )
+print(f'je suis ' + name)
+
---
+++ bool_op.py
@@ -0,0 +1,5 @@
+print(not 7)
+print(not 0)
+
+print(1 or 2 or 3)
+print(1 and 2 and 3)
